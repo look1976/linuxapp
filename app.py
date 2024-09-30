474f659c24b5b50eea36c0bb918a29bae5137451
Lukasz Buko <65955437+look1976@users.noreply.github.com>
2024-09-30 16:09:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,17 @@
-# version 0.1
-import os
+# app.py
 import random
 from flask import Flask
-from flask import render_template
 
 app = Flask(__name__)
 
-color_codes = {
-    "color01": "#4355e3",
-    "color02": "#1325e3",
-    "color03": "#3055e0",
-    "color04": "#0351e3",
-    "color05": "#2351e3",
-    "color06": "#0e55e3",
-}
+def get_random_color():
+    colors = ["Red", "Green", "Blue", "Yellow", "Purple", "Orange", "Pink", "Brown", "Black", "White"]
+    return random.choice(colors)
 
-color = os.environ.get('APP_COLOR') or random.choice(["color01", "color02", "color03", "color04", "color05", "color06"])
+@app.route('/')
+def display_random_color():
+    color = get_random_color()
+    return f'<h1 style="color:{color};">Random Color: {color}</h1>'
 
-@app.route("/")
-def main():
-    print(color)
-    return render_template('hello.html', name=socket.gethostname(), color=color_codes[color])
-
-@app.route('/color/<new_color>')
-def new_color(new_color):
-    return render_template('hello.html', name=socket.gethostname(), color=color_codes[color])
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
